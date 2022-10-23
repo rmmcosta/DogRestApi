6ddf32f032a4f17943308208bb596eb331dd0943
Ricardo Costa <ricardocosta101085@gmail.com>
2022-10-23 17:24:27 +0100

    added in memory authentication credentials

--- src/main/java/com/udacity/DogRestApi/config/SecurityConfig.java
+++ src/main/java/com/udacity/DogRestApi/config/SecurityConfig.java
@@ -1,18 +1,22 @@
 package com.udacity.DogRestApi.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
                 .authorizeRequests()
@@ -20,6 +24,20 @@ public class SecurityConfig {
                 .authenticated()
                 .and()
                 .httpBasic();
-        return http.build();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth)
+            throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password(encoder().encode("password"))
+                .roles("USER");
+    }
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
     }
 }
+
